4b44d945186eed6d977e350e0535f9bf4cdd428e
Avishka <avishkaranditha@gmail.com>
2018-05-27 20:34:55 +0530

    Code

---
+++ sse2/home.php
@@ -0,0 +1,30 @@
+
+<body>
+<link rel="stylesheet" type="text/css" href="theme_result.css">
+<link href="https://afeld.github.io/emoji-css/emoji.css" rel="stylesheet">
+
+<h2>
+<?php
+
+require_once 'token.php';
+
+
+$val = $_POST["token"];
+
+
+if(isset($_POST['updatepost'])){
+	if(token::checkToken($val,$_COOKIE['csrfCookie'])){
+		echo "Hey ".$_POST['updatepost']." ";
+	    echo "<i class=\"em em-ballot_box_with_check\"></i>";
+	}
+	else{
+	echo "wrong".$_COOKIE['csrfCookie'];
+	}
+}
+?>
+
+</h2>
+
+
+
+</body>
